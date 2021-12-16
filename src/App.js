48d6d0d1c0723ffe4a2c3911e456ac4a2bc2eb94
Ho Tien <tienhhv@miczone.asia>
2021-12-16 22:59:37 +0700

    add button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Button from './components/UI/button';
+import Main from './layouts/main';
 import Navbar from './layouts/navbar';
 
 function App() {
   return (
     <div className='App drop-shadow-xl'>
+      <Main>
+        <Button>Click</Button>
+      </Main>
       <Navbar />
     </div>
   );
---
+++ src/components/UI/button/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Button = ({ children, onClick }) => {
+  return <button onClick={onClick} className='h-20 w-full bg-gradient-to-br from-green-500 to-green-400 text-white text-4xl font-bold rounded-2xl'>{children}</button>;
+};
+
+export default Button;
