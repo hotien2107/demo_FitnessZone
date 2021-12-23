b54da51e667e5e02bbc8f3ba114792ff7c6765de
Ho Tien <hotien2107@gmail.com>
2021-12-23 10:18:50 +0700

    git pull

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ThumbnailBig from '../../components/UI/thumbnail/thumbnail-big';
 import ThumbnailSmall from '../../components/UI/thumbnail/thumbnail-small';
 
 const Home = () => {
