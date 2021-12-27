f78a6f185b174f7cc7cb393d6ef2340e5e523406
Ho Tien <hotien2107@gmail.com>
2021-12-27 13:28:45 +0700

    format code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import { Route, Routes, useLocation } from "react-router-dom";
-import "./App.css";
+import { Route, Routes, useLocation } from 'react-router-dom';
+import './App.css';
 
-import {
-  DETAIL_MAIN_ROUTES,
-  MAIN_ROUTES,
-  VIDEO_CALL,
-  VIDEO_CALL_ROUTES,
-} from "./constants";
+import { DETAIL_MAIN_ROUTES, MAIN_ROUTES, VIDEO_CALL, VIDEO_CALL_ROUTES } from './constants';
 
-import MainNavbar from "./layouts/navbar/main-navbar";
-import Chat from "./pages/chat";
-import Home from "./pages/home";
-import ListFriends from "./pages/list-friends";
-import InviteVideoCall from "./pages/invite-video-call";
-import ListFriendMenu from "./pages/list-friend-menu";
-import { VideoCallLayout } from "./layouts/VideoCallLayout";
-import { VideoCall } from "./pages/VideoCall";
-import { VideoCallExercise } from "./pages/VideoCallExercise";
+import MainNavbar from './layouts/navbar/main-navbar';
+import Chat from './pages/chat';
+import Home from './pages/home';
+import ListFriends from './pages/list-friends';
+import InviteVideoCall from './pages/invite-video-call';
+import ListFriendMenu from './pages/list-friend-menu';
+import { VideoCallLayout } from './layouts/VideoCallLayout';
+import { VideoCall } from './pages/VideoCall';
+import { VideoCallExercise } from './pages/VideoCallExercise';
 
 const isIn = (str1, str2) => {
   return str1.indexOf(str2) >= 0;
@@ -29,30 +24,21 @@ function App() {
   const isChat = isIn(currPath, DETAIL_MAIN_ROUTES.FRIENDS.CHAT);
 
   return (
-    <div className="App drop-shadow-xl ">
+    <div className='App drop-shadow-xl '>
       <Routes>
         <Route path={MAIN_ROUTES.HOME} element={<Home />} />
         <Route path={MAIN_ROUTES.FRIENDS.INDEX}>
           <Route index element={<ListFriends />} />
           <Route path={MAIN_ROUTES.FRIENDS.CHAT} element={<Chat />} />
-          <Route
-            path={MAIN_ROUTES.FRIENDS.CHATMENU}
-            element={<ListFriendMenu />}
-          />
-          <Route
-            path={MAIN_ROUTES.FRIENDS.INVITE}
-            element={<InviteVideoCall />}
-          />
+          <Route path={MAIN_ROUTES.FRIENDS.CHATMENU} element={<ListFriendMenu />} />
+          <Route path={MAIN_ROUTES.FRIENDS.INVITE} element={<InviteVideoCall />} />
         </Route>
         <Route path={VIDEO_CALL_ROUTES.HOME} element={<VideoCallLayout />}>
           <Route path={VIDEO_CALL_ROUTES.CHILD.VIDEO} element={<VideoCall />} />
-          <Route
-            path={VIDEO_CALL_ROUTES.CHILD.EXERCISE}
-            element={<VideoCallExercise />}
-          />
+          <Route path={VIDEO_CALL_ROUTES.CHILD.EXERCISE} element={<VideoCallExercise />} />
         </Route>
       </Routes>
-      {isChat ? "" : <MainNavbar />}
+      {isChat ? '' : <MainNavbar />}
     </div>
   );
 }
