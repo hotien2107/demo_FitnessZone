f14894c8e3e4d8cfec3c41f3fe0e98b73918a71c
minh-tri <cmtri2601@gmail.com>
2021-12-27 13:41:08 +0700

    list trainers

--- src/components/rate/rate.jsx
+++ src/components/rate/rate.jsx
@@ -4,9 +4,9 @@ const RateStar = ({ rate }) => {
   const rateStar = []
   for (let i = 0; i < 5; i++) {
     if (i < rate) {
-      rateStar.push(<MdStarRate className={'text-yellow-200 w-12 h-12'} />)
+      rateStar.push(<MdStarRate className={'text-yellow-300 w-12 h-12'} />)
     } else {
-      rateStar.push(<MdStarRate className={'text-stone-200 w-12 h-12'} />)
+      rateStar.push(<MdStarRate className={'text-stone-300 w-12 h-12'} />)
     }
   }
   return (
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@ export const MAIN_ROUTES = {
   HOME: "",
   FRIENDS: { INDEX: "list-friends", CHAT: "chat/:friendId",CHATMENU: 'menu', INVITE: 'menu/invite' },
   EXERCISES: { INDEX: 'exercises', TRAINER_CLASS: 'trainer-exercise'},
-  TRAINERS: { INDEX: 'trainers'}
+  TRAINERS: { INDEX: 'list-trainers'}
 };
 
 export const DETAIL_MAIN_ROUTES = {
